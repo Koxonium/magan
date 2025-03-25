01de3b2332debf29b6d3decc1a0a74f82f666ebf
= <toth.levente.21i@egressy.info>
2025-03-25 10:42:16 +0100

    Add project files.

---
+++ WpfApp8/Window1.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace magan
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        ServerConnection connection;
+
+        public Window1(ServerConnection connection)
+        {
+            InitializeComponent();
+            this.connection = connection;
+            start();
+        }
+        void start()
+        {
+            asd();
+        }
+        async void create(object s, EventArgs e)
+        {
+            bool temp = await connection.createPerson(NameInput.Text, Convert.ToInt32(AgeInput.Text));
+            if (temp)
+            {
+                MessageBox.Show("Sikeres létrehozás");
+                asd();
+            }
+        }
+        async void asd()
+        {
+
+            NameStackPanel.Children.Clear();
+            AgeStackPanel.Children.Clear();
+            List<string> allnames = await connection.AllNames();
+            foreach (string item in allnames)
+            {
+                NameStackPanel.Children.Add(new TextBlock() { Text = item });
+                TextBlock nameLabel = new TextBlock();
+                nameLabel.Text = item;
+                NameStackPanel.Children.Add(nameLabel);
+                Button delbutton = new Button();
+                delbutton.Content = "X";
+                delbutton.Click += async (s, e) =>
+                {
+                    bool temp = await connection.deletePerson(nameLabel.Text);
+                    if (temp)
+                    {
+                        asd();
+                        MessageBox.Show("törölve!");
+                    }
+                };
+                dels.Children.Add(delbutton);
+                
+            }
+            List<string> allages = await connection.AllAges();
+            foreach (string item in allages)
+            {
+                AgeStackPanel.Children.Add(new TextBlock() { Text = item });
+            }
+        }
+    }
+}
